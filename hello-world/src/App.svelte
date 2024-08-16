d4cd456324a270568338ddab79cdf800db4daeea
sathish257216 <sathish.257216@gmail.com>
2024-08-16 15:12:59 -0400

    basics, binding, directive, compoennts, events, loop, forms, bind

---
+++ hello-world/src/App.svelte
@@ -0,0 +1,102 @@
+<script>
+	import UserList from "./components/UserList.svelte";
+	
+	export let name;
+	const msg = "<b>Welcome to the Svelte</b>";
+	const isAttr = 'msg-text';
+	const isDisabled = false;
+	let num = 1;
+
+	let signupForm = {
+		name: "signup",
+		email: "",
+		mobile: ""
+	}
+
+	$: nameEmail = signupForm.name + " " + signupForm.email;
+	const handleClick = () => {
+		console.log("handleClick ");
+	}
+
+	const onSubmitForm = () => {
+		console.log('onSubmitForm', signupForm);
+	}
+</script>
+
+<main>
+	<h1>Hello {name}!</h1>
+	<span>{ @html msg}</span>
+	<p id={isAttr}>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<button disabled={isDisabled} class:disable-btn={isDisabled} on:click={handleClick}>Next</button>
+
+	<br/>
+	<div class="parent-container">
+		<div>
+			{#if num == 0}
+				<h3> Number is Zero</h3>
+			{:else if num < 0}
+				<h3> Number is negative</h3>
+			{:else if num > 100}
+				<h3> Number is greater than 100</h3>
+			{:else}
+				<h3> Number is less than 100</h3>
+			{/if}
+		</div>
+		<br/>
+		<UserList pageName={'Home'} on:close={($event)=> console.log("custom Event", $event.detail)}/>
+		<br/>
+		<div class="signup-form">
+			<h2>Signup</h2>
+			<pre>{JSON.stringify(signupForm)	}</pre>
+			<form on:submit={onSubmitForm}>
+			<div class="form-field">
+				<label for="name">Name</label>
+				<input type="text" id="name" bind:value={signupForm.name} />
+			</div>
+			<div class="form-field">
+				<label for="email">Email</label>
+				<input type="text" id="email" bind:value={signupForm.email} />
+			</div>
+			<div class="form-field">
+				<label for="mobile">Mobile</label>
+				<input type="text" id="mobile" bind:value={signupForm.mobile} />
+			</div>
+
+			<button type="submit" class="submit-btn" id="submit-form">Submit</button>
+			</form>
+		</div>
+		</div>
+			<br />
+		<div>
+			<h6>Reactive Decleration</h6>
+			<span>{nameEmail}</span>
+		</div>
+</main>
+
+<style>
+	main {
+		text-align: center;
+		padding: 1em;
+		max-width: 240px;
+		margin: 0 auto;
+	}
+	.parent-container {
+		padding: 20px;
+	}
+	.name-list {
+		display: flex;
+		flex-direction: column;
+	}
+	h1 {
+		color: #ff3e00;
+		text-transform: uppercase;
+		font-size: 4em;
+		font-weight: 100;
+	}
+
+	@media (min-width: 640px) {
+		main {
+			max-width: none;
+		}
+	}
+</style>
---
+++ hello-world/src/components/UserList.svelte
@@ -0,0 +1,19 @@
+<script>
+    import {createEventDispatcher} from "svelte";
+
+    export let pageName = '';
+    let users = ["Smith", "Jhon", "Tom"];
+
+    const dispatch = createEventDispatcher();
+</script>
+<main>
+    <div class="name-list">
+        <h3>{pageName}</h3>
+        <h5> User List</h5>
+        {#each users as user, index (user)}
+            <div><span>{index+1}. {user}</span></div>
+        {/each}
+
+        <button type="button" on:click={() => dispatch("close", 'data')}>Send Custom Event</button>
+    </div>
+</main>
